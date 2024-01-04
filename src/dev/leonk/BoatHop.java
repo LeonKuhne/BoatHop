c602fc1bd57627dcf39acebdeb0d13c3425d6b8d
Leon Kuhne <code@leonk.dev>
2024-01-03 19:38:11 -0600

    finished up plugin

--- src/dev/leonk/BoatHop.java
+++ src/dev/leonk/BoatHop.java
@@ -1,5 +1,6 @@
 package dev.leonk;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -24,20 +25,26 @@ public class BoatHop extends JavaPlugin implements Listener {
   }
 
   @Override
-  public void onDisable() {
-    saveConfig();
-  }
-
-  @Override
   public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-    if (cmd.getName() != "boathop") return false;
-    if (args.length < 2) return false;
-    if (!sender.hasPermission("boathop.config")) return false;
-    String subcommand = args[0];
-    if (!config.contains(subcommand)) return false;
-    double value = Double.parseDouble(args[1]);
-    config.set(subcommand, value);
-    return false;
+    if (!cmd.getName().equals("boathop")) return false;
+    if (!sender.hasPermission("boathop.config")) {
+      sender.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
+      return true;
+    }
+    // validate command
+    switch (args.length) {
+      case 0: break;
+      case 2:
+        if (!config.contains(args[0])) return false;
+        // update config
+        try { updateConfig(args[0], Double.parseDouble(args[1])); }
+        catch (NumberFormatException e) { return false; }
+        break;
+      default: return false;
+    }
+    // show settings
+    showConfig(sender);
+    return true;
   }
 
   @EventHandler
@@ -51,12 +58,25 @@ public class BoatHop extends JavaPlugin implements Listener {
     if (!(player.getVehicle() instanceof Boat)) return;
     if (player.getLocation().getBlock().getRelative(0, 0, 0).getType() != Material.WATER) return;
     Boat boat = (Boat) player.getVehicle();
-    // get the forward direction
-    Vector direction = boat.getLocation().getDirection();
-    // increase boats velocity
+    Vector boatDirection = boat.getLocation().getDirection();
+    // update boats velocity
     Vector velocity = boat.getVelocity()
-      .add(direction.multiply(config.getDouble("boost")))
+      .add(boatDirection.multiply(config.getDouble("boost")))
       .add(new Vector(0., config.getDouble("jump"), 0.));
     boat.setVelocity(velocity);
+  }
+
+  private void showConfig(CommandSender sender) {
+    StringBuilder msg = new StringBuilder();
+    msg.append(String.format("%s%sBoatHop Settings:%s", ChatColor.GOLD, ChatColor.UNDERLINE, ChatColor.RESET));
+    for (String key : config.getKeys(false)) {
+      msg.append(String.format("\n%s%s: %s%s%s%s", ChatColor.GRAY, key, ChatColor.BOLD, ChatColor.GREEN, config.get(key), ChatColor.RESET));
+    }
+    sender.sendMessage(msg.toString());
+  }
+
+  private void updateConfig(String key, Double value) {
+    config.set(key, value);
+    saveConfig();
   }
 }
